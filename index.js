89b2916c805ac2992497888e452a350fbe31d5e8
Estelle K <e.keaton@live.com>
2019-03-24 17:29:08 -0700

    Finalizing portfolio

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'; 
+
+//when the page loads the circle append to the DOM
+function circlesAppear() {
+$('.circles').append(`<li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+      <li></li>
+      <li></li>`)};
+
+
+//hamburger menu appears
+function hamburger() {
+$('.hamburger').click(event => {
+ event.preventDefault();
+ $('.mobileNav').removeClass('hidden');
+});
+
+}
+
+$(hamburger);
+$(circlesAppear);
