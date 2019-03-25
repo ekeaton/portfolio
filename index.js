fcf135cdf76c656b71d4e21437275a6733d5489f
Estelle K <e.keaton@live.com>
2019-03-25 09:52:59 -0700

    Updated styling and fixed bug

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict'; 
 
+$(document).ready(function() {
+
 //when the page loads the circle append to the DOM
+$(circlesAppear);
 function circlesAppear() {
 $('.circles').append(`<li></li>
        <li></li>
@@ -15,13 +18,24 @@ $('.circles').append(`<li></li>
 
 
 //hamburger menu appears
+$(hamburger);
 function hamburger() {
 $('.hamburger').click(event => {
  event.preventDefault();
  $('.mobileNav').toggleClass('hidden');
+      });
+
+   }
+
+  //remove the opened hamburger menu
+  //when the viewport expands from mobile
+  $(window).resize(function () {
+      var viewportWidth = $(window).width();
+      if (viewportWidth < 768) {
+              $(".mobileNav").addClass("hidden");
+      }
+  });
+
 });
 
-}
 
-$(hamburger);
-$(circlesAppear);
